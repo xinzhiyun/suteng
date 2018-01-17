f36b5597be40b75fd082d132b47720f4e1a1eca1
吴智彬 <519002008@qq.com>
2018-01-17 09:14:14 +0800

    更新分销商系统

--- project/Application/Admin/Controller/VendorsController.class.php
+++ project/Application/Admin/Controller/VendorsController.class.php
@@ -1,6 +1,6 @@
 <?php
 namespace Admin\Controller;
-
+use Home\Controller\DimensionController;
 use Think\Controller;
 
 class VendorsController extends CommonController
@@ -55,7 +55,10 @@ class VendorsController extends CommonController
         $this->ajaxReturn($message);
     }
 
-    // 经销商添加
+    /**
+     * [vendor_add 经销商添加]
+     * @return [type] [description]
+     */
     public function vendor_add(){
         if(IS_POST){
             $vendors = D("vendors"); // 实例化User对象
@@ -77,6 +80,8 @@ class VendorsController extends CommonController
             $this->display();
         }
     }
+
+
 
     // 简单的验证一下不能有空的数据
     public function check_empty($data)
@@ -130,5 +135,165 @@ class VendorsController extends CommonController
         // 逻辑暂时为空
         $res = M('vendors')->where("idcard='{$add_info['idcard']}'")->getField('idcard');
         return $res;
+    }
+
+    /**
+     * [company_add 分公司添加]
+     * @return [type] [description]
+     */
+    public function company_add(){
+        if(IS_POST){
+            // 没有上传图片报错信息组
+            $emptyError = array(
+                'positive' =>  '身份证正面图片必须上传',
+                'opposite' =>  '身份证反面图片必须上传',
+                'handheld' =>  '手持身份证正面图片必须上传',
+                'licence'  =>  '营业执照图片必须上传',
+                'protocol' =>  '协调文件图片必须上传',
+            );
+
+            // 图片超出上传文件大小报错组
+            $sizeError = array(
+                'positive' =>  '身份证正面图片超出允许上传文件大小3M',
+                'opposite' =>  '身份证反面图片超出允许上传文件大小3M',
+                'handheld' =>  '手持身份证正面图片超出允许上传文件大小3M',
+                'licence'  =>  '营业执照图片超出允许上传文件大小3M',
+                'protocol' =>  '协调文件图片超出允许上传文件大小3M',
+            );
+
+            // 图片超出上传文件大小报错组
+            $typeError = array(
+                'positive' =>  '身份证正面图片类型错误,允许jpg, gif, png, jpeg',
+                'opposite' =>  '身份证反面图片类型错误,允许jpg, gif, png, jpeg',
+                'handheld' =>  '手持身份证正面图片类型错误,允许jpg, gif, png, jpeg',
+                'licence'  =>  '营业执照图片类型错误,允许jpg, gif, png, jpeg',
+                'protocol' =>  '协调文件图片类型错误,允许jpg, gif, png, jpeg',
+            );
+
+            // 允许的图片类型
+            $type = array(
+                "image/jpg",
+                "image/png",
+                "image/gif",
+                "image/jpeg",
+            );
+
+            // 遍历上传图片信息
+            foreach ($_FILES as $key => $value) {
+                // 不上传图片检测
+                if($_FILES[$key]['error'] == 4){
+                    // 反馈XX图片必须上传
+                    $this->error($emptyError[$key]);
+                    return false;
+                }
+
+                // 图片大小检测
+                if($_FILES[$key]['size'] > 3145728){
+                    // 反馈XX图片大小错误
+                    $this->error($sizeError[$key]);
+                    return false;
+                }
+
+                // 图片类型检测
+                if(!in_array($_FILES[$key]['type'], $type)){
+                    // 反馈XX图片类型错误
+                    $this->error($typeError[$key]);
+                    return false;
+                }
+            }
+            
+            // 处理图片
+            $info = $this->upload();
+            // 检查图片是否上传成功
+            if($info){
+                $vendors = D("vendors"); // 实例化User对象
+                $data = $vendors->create();
+                if (!$data){ // 创建数据对象
+                     // 如果创建失败 表示验证没有通过 输出错误提示信息
+                     $this->success($vendors->getError());
+                     return false;
+                }else{
+                    $getCode            = new DimensionController;
+                    $ticket             = $getCode->office();
+                    $newData            = array_merge($data,$info); // 将图片合并入数据中
+                    $newData['code']    = $this->vendor_code();     // 将分公司唯一标识加人数组中
+                    $newData['leavel']  = 1;
+                    $newData['status']  = 3;
+                    $newData = array_merge($newData,$ticket);
+                    //dump($newData);die;
+                    // 验证通过 写入新增数据
+                    if($vendors->add($newData)){
+                        $this->success('申请已经提交，待审核中');
+                        return false;
+                    }else{
+                        $this->error('提交失败了！');
+                        return false;
+                    }
+                }
+            }else{
+                $this->error('图片上传失败，请重新上传！');
+                return false;
+            }
+        }else{
+            $this->display();
+        }
+    }
+
+    /**
+     * [company_list 查看分公司列表]
+     * @return [type] [description]
+     */
+    public function company_list()
+    {
+        $data = D('vendors')->companyList();
+        $assign = [
+            'data' => $data,
+        ];
+        $this->assign($assign);
+        $this->display();
+    }
+
+    /**
+     * [upload 图片上传]
+     * @return [type] [description]
+     */
+    public function upload()
+    {
+        $upload = new \Think\Upload();// 实例化上传类
+        $upload->maxSize   =     3145728;// 设置附件上传大小3MB
+        $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+        $upload->rootPath  =     './Public/'; // 设置附件上传根目录
+        $upload->savePath  =     '/Vendors/'; // 设置附件上传（子）目录
+        // 上传文件 
+        $info   =   $upload->upload();
+        if(!$info) {// 上传错误提示错误信息
+            return false;
+            // $this->error($upload->getError());
+        }else{
+            // 上传成功
+            foreach ($info as $key=>$file) {
+                // dump($info);die;
+                $pic[$key] = $file['savepath'].$file['savename'];
+            }
+            // $this->success('上传成功！');
+            return $pic;
+        }
+    }
+
+    /**
+     * 获取分销商唯一标识
+     */
+    public function vendor_code()
+    {        
+        do {
+          // 生成分销商唯一标识
+          $code = strtoupper(substr(md5(mt_rand(100000, 999999)), mt_rand(1,9), 6));
+          //查询销商标识是否存在
+          $oid = M('vendors')->where("`code`='{$code}'")->field('id')->find();
+          // 如果订单号已存在再重新获取一次
+        } while ($oid);
+
+        // 绝对唯一的32位订单ID号
+        return $code;
     }
 }
--- project/Application/Home/Controller/DimensionController.class.php
+++ project/Application/Home/Controller/DimensionController.class.php
@@ -14,7 +14,7 @@ class DimensionController extends Controller
 	 * 永久票据有效时间：长期有效
 	 * 使用票据：<img src="https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket='.$jsoninfo.'" alt="二维码票据">
 	 */
-    public function vedor($num,$type=false)
+    public function code($num,$type=false)
     {
     	// 实例化微信JSSDK类对象
         $wxJSSDK = new \Org\Util\WeixinJssdk;
@@ -34,6 +34,95 @@ class DimensionController extends Controller
         $jsoninfo = json_decode($result,true)['ticket'];
         // 返回获取二维码票据
         return $jsoninfo;
+    }
+
+	/**
+	 * [vedor 吴智彬]
+	 * @return [string]     $jsoninfo   [带参数二维码票据]
+	 * 永久票据有效时间：长期有效
+	 * 使用票据：<img src="https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket='.$jsoninfo.'" alt="二维码票据">
+	 */
+    public function office()
+    {
+    	// 实例化微信JSSDK类对象
+        $wxJSSDK = new \Org\Util\WeixinJssdk;
+        // 调用获取公众号的全局唯一接口调用凭据
+        $accessToken = $wxJSSDK->getAccessToken();
+        // 请求微信带参数二维码
+        $url = "https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token={$accessToken}";
+        // 查询二维码遍号
+        $num = M('dimensionNum')->where('`id`=1')->find()['vendors_num'];
+
+        // 分销商二维码下标定义
+        $ticket = array('vendor_a','vendor_b','vendor_c');
+        // 创建空数组准备装二维码
+        $vedors = array();
+        // 定义索引
+        $j = 0;
+        // 循环生成3个分销商邀请码
+        for($i=$num;$i<$num+3;$i++){
+	        // 永久二维码请求数据格式
+	        $data = '{"action_name":"QR_LIMIT_SCENE","action_info":{"scene":{"scene_id":'.$i.'}}';
+	        // 发送请求
+	        $result = $this->httpPost($url,$data);
+	        // 解析JSON格式数据
+	        $jsoninfo = json_decode($result,true)['ticket'];
+	        // 返回获取二维码票据
+	        $vedors[$ticket[$j]] = $jsoninfo;
+	        // 索引自增
+	        $j++;
+        }
+
+        // 实例化二维码对象
+        $dimension = M('dimension');
+        // 开启事务
+        $dimension->startTrans();
+
+        // 填写A级分销商二维码信息表
+		$vendor_a['ticket'] 	= $vedors['vendor_a'];
+		$vendor_a['recommend']	= 3;
+		$vendor_a['leavel']		= 0;	
+		$vendor_a['effective']	= 0;
+		$vendor_a['addtime']	= time();
+		$vendor_a['updatetime']	= $vendor_a['addtime'];
+		$resa = $dimension->add($vendor_a);
+
+		// 填写B级分销商二维码信息表
+		$vendor_b['ticket'] 	= $vedors['vendor_b'];
+		$vendor_b['recommend']	= 3;
+		$vendor_b['leavel']		= 1;	
+		$vendor_b['effective']	= 0;
+		$vendor_b['addtime']	= time();
+		$vendor_b['updatetime']	= $vendor_b['addtime'];
+		$resb = $dimension->add($vendor_b);
+
+		// 填写B级分销商二维码信息表
+		$vendor_c['ticket'] 	= $vedors['vendor_c'];
+		$vendor_c['recommend']	= 3;
+		$vendor_c['leavel']		= 2;	
+		$vendor_c['effective']	= 0;
+		$vendor_c['addtime']	= time();
+		$vendor_c['updatetime']	= $vendor_c['addtime'];
+		$resc = $dimension->add($vendor_c);
+
+		// 修改分销商二维码参数
+		$saveDate['vendors_num'] = $num+3;
+		$resNum = M('dimensionNum')->where('id=1')->save($saveDate);
+		//dump($resNum);die;
+		// 如果二唯码全部创建成果，并修改参数成功
+		if($resa && $resb && $resc && $resNum){
+			// 执行事务
+			$dimension->commit();
+	        // 返回获取二维码票据
+	        return $vedors;
+		}else{
+			// 事务回滚
+			$dimension->rollback();
+			// 二维码创建失败
+			return false;
+		}
+
+
     }
 
     /**
