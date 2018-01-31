1db8d558671aa8c1f3a6eba09dcaee5fc17a220b
吴智彬 <519002008@qq.com>
2018-01-31 11:24:47 +0800

    更新支付系统微信标识

--- project/Application/Home/Controller/PaymentSystemController.class.php
+++ project/Application/Home/Controller/PaymentSystemController.class.php
@@ -141,7 +141,7 @@ class PaymentSystemController extends CommonController
         // 冲值测试额1分钱
         $money = 1;
         // 用户在公众号的唯一ID
-        $openId = $_SESSION['user']['open_id'];
+        $openId = $_SESSION['open_id'];
 
 
         //微信examle的WxPay.JsApiPay.php
