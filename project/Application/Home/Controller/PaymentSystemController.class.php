09a0c8e4662424f2d6e652516cd5be14143a58a0
吴智彬 <519002008@qq.com>
2018-01-31 18:10:11 +0800

    更新订单支付接口

--- project/Application/Home/Controller/PaymentSystemController.class.php
+++ project/Application/Home/Controller/PaymentSystemController.class.php
@@ -76,6 +76,7 @@ class PaymentSystemController extends CommonController
     	$this->display();
     }
 
+
     /**
      * [paytofailed 支付失败]
      * @return [type] [description]
@@ -83,6 +84,34 @@ class PaymentSystemController extends CommonController
     public function paytofailed()
     {
         $this->display();
+    }
+
+    /**
+     * [paytofailed 订单支付]
+     * @return [type] [description]
+     */
+    public function orderPay()
+    {
+        if(IS_POST){
+            // 准备订单查询数据
+            $showWhere['order'] = I('post.order');
+            // 查询订单表
+            $orderData = M('shop_order')->where($showWhere)->find();
+
+            // 判断订单是否存在
+            if($orderData){
+                // 订单金额
+                $money = $orderData['g_price'];
+                // 订单号码
+                $order_id = $orderData['order_id'];
+                // 订单描述
+                $content = '速腾商城商品购买';
+                $this->uniformOrder($money,$order_id,$content);
+            }else{
+                // 订单不存在
+                echo -1;
+            }
+        }
     }
 
     /**
