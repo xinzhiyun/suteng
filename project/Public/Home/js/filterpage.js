4b9979c9fa39106419d0127d553666647c849dc2
washi <1986364669@qq.com>
2018-02-08 16:59:46 +0800

    滤芯页面js更新;订单页面js更新

--- project/Public/Home/js/filterpage.js
+++ project/Public/Home/js/filterpage.js
@@ -3,57 +3,60 @@ $(function(){
 	var filterName='';
 	var filterNumber=0;
 	//页面加载时获取滤芯数据
-	// var data = JSON.parse($(".filterData").val()); 
+	var data = JSON.parse($(".filterData").val());//滤芯剩余量数据
+	var res = JSON.parse($(".lvxin_data").val()).filterInfo;//滤芯详情数据
+	// console.log(res)
 	//页面加载时获取数据
-    var data={
-        "id":"1",
-        "deviceid":"228733445596778",
-        "devicestause":"0",
-        "reflow":"34000",
-        "reday":"85",
-        "rawtds":"75",
-        "puretds":"42",
-        "temperature":"24",
-        "reflowfilter1":"10000",
-        "redayfilter1":"1000",
-        "reflowfilter2":"50",
-        "redayfilter2":"1000",
-        "reflowfilter3":"60",
-        "redayfilter3":"1000",
-        "reflowfilter4":"10000",
-        "redayfilter4":"1000",
-        "reflowfilter5":"40",
-        "redayfilter5":"1000",
-        "reflowfilter6":"80",
-        "redayfilter6":"1000",
-        "reflowfilter7":"45",
-        "redayfilter7":"1000",
-        "reflowfilter8":"76",
-        "redayfilter8":"42",
-        "leasingmode":"2",
-        "alivestause":"1",
-        "roomtemperature":"26",
-         "sumflow":"24",
-        "sumday":"65",
-        "phval":"3.8",
-        "coldwater":"20",
-        "hotwater":"95",
-        "filtermode":"0",
-        "device":"GPRS",
-        "iccid":"11223344556677889900",
-        "csq":"90",
-        "loaction":"10020�����",
-        "addtime":"1513238294",
-        "updatetime":"1513299086"
-    };
-    var res = [
-		{filtername:'M15UCC',timelife:1000,flowlife:30,introduce: '原装正品净水器滤芯M15UCC 0.5微米高密道4级过滤'},
-		{filtername:'HU603-5a',timelife:1000,flowlife:60,introduce: '正品净水器家用直饮机HU603-5a全套原装滤芯'},
-		{filtername:'RO滤芯',timelife:1000,flowlife:70,introduce: '原装JR1550T-RO RO/C+UF/PP+C五级过滤'},
-		{filtername:'BH2',timelife:1000,flowlife:50,introduce: '净水器滤芯主滤芯耗材家用直饮净水器主滤芯BH2'},
-		{filtername:'RO-185I',timelife:1000,flowlife:50,introduce: '沁园净水器滤芯RO-185I/DT/F PP棉QG-U-1003/1004 套装 10寸 通用'},
-		{filtername:'AO史密斯',timelife:1000,flowlife:90,introduce: 'AO史密斯50D1 厨房过滤家用净水器 专利反渗透滤芯直饮净水机'}
-	];
+    // var data={
+    //     "id":"1",
+    //     "deviceid":"228733445596778",
+    //     "devicestause":"0",
+    //     "reflow":"34000",
+    //     "reday":"85",
+    //     "rawtds":"75",
+    //     "puretds":"42",
+    //     "temperature":"24",
+    //     "reflowfilter1":"10000",
+    //     "redayfilter1":"1000",
+    //     "reflowfilter2":"50",
+    //     "redayfilter2":"1000",
+    //     "reflowfilter3":"60",
+    //     "redayfilter3":"1000",
+    //     "reflowfilter4":"10000",
+    //     "redayfilter4":"1000",
+    //     "reflowfilter5":"40",
+    //     "redayfilter5":"1000",
+    //     "reflowfilter6":"80",
+    //     "redayfilter6":"1000",
+    //     "reflowfilter7":"45",
+    //     "redayfilter7":"1000",
+    //     "reflowfilter8":"76",
+    //     "redayfilter8":"42",
+    //     "leasingmode":"2",
+    //     "alivestause":"1",
+    //     "roomtemperature":"26",
+    //      "sumflow":"24",
+    //     "sumday":"65",
+    //     "phval":"3.8",
+    //     "coldwater":"20",
+    //     "hotwater":"95",
+    //     "filtermode":"0",
+    //     "device":"GPRS",
+    //     "iccid":"11223344556677889900",
+    //     "csq":"90",
+    //     "loaction":"10020�����",
+    //     "addtime":"1513238294",
+    //     "updatetime":"1513299086"
+    // };
+    // console.log(res);
+ //    var res = [
+	// 	{filtername:'M15UCC',timelife:1000,flowlife:30,introduce: '原装正品净水器滤芯M15UCC 0.5微米高密道4级过滤'},
+	// 	{filtername:'HU603-5a',timelife:1000,flowlife:60,introduce: '正品净水器家用直饮机HU603-5a全套原装滤芯'},
+	// 	{filtername:'RO滤芯',timelife:1000,flowlife:70,introduce: '原装JR1550T-RO RO/C+UF/PP+C五级过滤'},
+	// 	{filtername:'BH2',timelife:1000,flowlife:50,introduce: '净水器滤芯主滤芯耗材家用直饮净水器主滤芯BH2'},
+	// 	{filtername:'RO-185I',timelife:1000,flowlife:50,introduce: '沁园净水器滤芯RO-185I/DT/F PP棉QG-U-1003/1004 套装 10寸 通用'},
+	// 	{filtername:'AO史密斯',timelife:1000,flowlife:90,introduce: 'AO史密斯50D1 厨房过滤家用净水器 专利反渗透滤芯直饮净水机'}
+	// ];
 	// 判断内容是否在购买超出，不超出则溢出隐藏
 	$("#content")
 	.css({height: lvxinArr[2].height, 
@@ -61,8 +64,6 @@ $(function(){
 		? 'visible'
 		: 'hidden'
 	});
-	var lvxin_data = JSON.parse($(".lvxin_data").val()).filterInfo;
-        // console.log(data);
 	// 当当前设备为零售模式时候，不显示滤芯购买页面
 	if(data!=null){
 		if(data.leasingmode=="0"){
@@ -142,157 +143,161 @@ $(function(){
 	var reflowpercent;//滤芯剩余流量百分比
 	
 	for(var i=0; i<res.length; i++){
-		timelife = (res[i].timelife?res[i].timelife:0);
-		flowlife = (res[i].flowlife?res[i].flowlife:0);
-		reday = (data['redayfilter'+(i+1)]?data['redayfilter'+(i+1)]:0);
-		reflow = (data['reflowfilter'+(i+1)]?data['reflowfilter'+(i+1)]:0);
-		redaypercent = (((data['redayfilter'+(i+1)]/res[i].timelife)*100).toFixed(2)>100)?100:(((data['redayfilter'+(i+1)]/res[i].timelife)*100).toFixed(2));
-		reflowpercent = (((data['reflowfilter'+(i+1)]/res[i].flowlife)*100).toFixed(0)>100)?100:(((data['reflowfilter'+(i+1)]/res[i].flowlife)*100).toFixed(0));
-	    if(filtermode =="0"){//时常
-	        // 遍历膜的详情, 不存在则不遍历
-	        if(res[i].filtername){
-	            moHTML += '<li class="_padding">'+
-	                '<p><span class="">'+ res[i].filtername +
-	                '</span><span class="_float_right">'+
-	                (redaypercent?redaypercent:0)
-	                +'%</span></p>'+
-	                '<p class="_text_align">'+
-	                    '<div class="progress _progress _width">'+
-	                       '<div class="iconfont icon-kuang1"><span name='+ 'filter'+i +'></span></div>'+
-	                      '<div class="progress-bar progress-bar-striped" role="progressbar" style="width:'+
-	                     (redaypercent?redaypercent:0)+
-	                      '%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>'+
-	                    '</div>'+
-	                '</p>'+
-	            '</li>';
-	        }
-	        //滤芯简介
-	        if(res[i].introduce){
-	            lvxinIntroduceHTML += '<div>'+
-	                '<h3>'+
-	                    res[i].filtername +
-	                '</h3>'+
-	                '<p>'+
-	                    res[i].introduce +
-	                '</p>'+
-	                '<p>'+
-	                    '<span class="_font_weigth">总寿命：'+ timelife +
-	                    '天</span>'+
-	                    '<span class="_float_right _font_weigth">剩余：'+ reday +
-	                    '天</span>'+
-	                '</p>'+
-	            '</div>';
-	        }
-	    }else if(filtermode == "1"){//流量
-	        //流量模式
-	         // 遍历滤芯详情, 不存在则不遍历
-	        if(res[i].filtername){
+		
+		if(res[i]){
+			console.log(res[i].introduce);
+			timelife = (res[i].timelife?res[i].timelife:0);
+			flowlife = (res[i].flowlife?res[i].flowlife:0);
+			reday = (data['redayfilter'+(i+1)]?data['redayfilter'+(i+1)]:0);
+			reflow = (data['reflowfilter'+(i+1)]?data['reflowfilter'+(i+1)]:0);
+			redaypercent = (((data['redayfilter'+(i+1)]/res[i].timelife)*100).toFixed(2)>100)?100:(((data['redayfilter'+(i+1)]/res[i].timelife)*100).toFixed(2));
+			reflowpercent = (((data['reflowfilter'+(i+1)]/res[i].flowlife)*100).toFixed(0)>100)?100:(((data['reflowfilter'+(i+1)]/res[i].flowlife)*100).toFixed(0));
+		    if(filtermode =="0"){//时常
+		        // 遍历膜的详情, 不存在则不遍历
+		        if(res[i].filtername){
+		            moHTML += '<li class="_padding">'+
+		                '<p><span class="">'+ res[i].filtername +
+		                '</span><span class="_float_right">'+
+		                (redaypercent?redaypercent:0)
+		                +'%</span></p>'+
+		                '<p class="_text_align">'+
+		                    '<div class="progress _progress _width">'+
+		                       '<div class="iconfont icon-kuang1"><span name='+ 'filter'+i +'></span></div>'+
+		                      '<div class="progress-bar progress-bar-striped" role="progressbar" style="width:'+
+		                     (redaypercent?redaypercent:0)+
+		                      '%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>'+
+		                    '</div>'+
+		                '</p>'+
+		            '</li>';
+		        }
+		        //滤芯简介
+		        if(res[i].introduce){
+		            lvxinIntroduceHTML += '<div>'+
+		                '<h3>'+
+		                    res[i].filtername +
+		                '</h3>'+
+		                '<p>'+
+		                    res[i].introduce +
+		                '</p>'+
+		                '<p>'+
+		                    '<span class="_font_weigth">总寿命：'+ timelife +
+		                    '天</span>'+
+		                    '<span class="_float_right _font_weigth">剩余：'+ reday +
+		                    '天</span>'+
+		                '</p>'+
+		            '</div>';
+		        }
+		    }else if(filtermode == "1"){//流量
+		        //流量模式
+		         // 遍历滤芯详情, 不存在则不遍历
+		        if(res[i].filtername){
 
-	            moHTML += '<li class="_padding">'+
-	                '<p><span class="">'+ res[i].filtername +
-	                '</span><span class="_float_right">'+
-	                (reflowpercent?reflowpercent:0)
-	                +'%</span></p>'+
-	                '<p class="_text_align">'+
-	                    '<div class="progress _progress _width">'+
-	                       '<div class="iconfont icon-kuang1"><span name='+ 'filter'+i +'></span></div>'+
-	                      '<div class="progress-bar progress-bar-striped" role="progressbar" style="width:'+
-	                     (reflowpercent?reflowpercent:0) +
-	                      '%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>'+
-	                    '</div>'+
-	                '</p>'+
-	            '</li>';
-	        }
+		            moHTML += '<li class="_padding">'+
+		                '<p><span class="">'+ res[i].filtername +
+		                '</span><span class="_float_right">'+
+		                (reflowpercent?reflowpercent:0)
+		                +'%</span></p>'+
+		                '<p class="_text_align">'+
+		                    '<div class="progress _progress _width">'+
+		                       '<div class="iconfont icon-kuang1"><span name='+ 'filter'+i +'></span></div>'+
+		                      '<div class="progress-bar progress-bar-striped" role="progressbar" style="width:'+
+		                     (reflowpercent?reflowpercent:0) +
+		                      '%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>'+
+		                    '</div>'+
+		                '</p>'+
+		            '</li>';
+		        }
 
-	        //滤芯简介
-	        if(res[i].introduce && data.reflowfilter+[i+1]){
-	            lvxinIntroduceHTML += '<div>'+
-	                '<h3>'+
-	                    res[i].filtername +
-	                '</h3>'+
-	                '<p>'+
-	                    res[i].introduce +
-	                '</p>'+
-	                '<p>'+
-	                    '<span class="_font_weigth">总寿命：'+ flowlife +
-	                    'L</span>'+
-	                    '<span class="_float_right _font_weigth">剩余：'+ reflow +
-	                    'L</span>'+
-	                '</p>'+
-	            '</div>';
-	        }
-	    }else if(filtermode == "2"){//时常&流量
-	        // 遍历膜的详情, 不存在则不遍历
-	       if(redaypercent > reflowpercent){//时间剩余比例小则显示时间
-	            if(res[i].filtername){
-	                moHTML += '<li class="_padding">'+
-	                    '<p><span class="">'+ res[i].filtername +
-	                    '</span><span class="_float_right">'+
-	                    (redaypercent?redaypercent:0) +
-	                    '%</span></p>'+
-	                    '<p class="_text_align">'+
-	                        '<div class="progress _progress _width">'+
-	                           '<div class="iconfont icon-kuang1"><span name='+ 'filter'+i +'></span></div>'+
-	                          '<div class="progress-bar progress-bar-striped" role="progressbar" style="width:'+
-	                         (redaypercent?redaypercent:0) +
-	                          '%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>'+
-	                        '</div>'+
-	                    '</p>'+
-	                '</li>';
-	            }
-	            //滤芯简介
-	            if(res[i].introduce){
-	                lvxinIntroduceHTML += '<div>'+
-	                    '<h3>'+
-	                        res[i].filtername +
-	                    '</h3>'+
-	                    '<p>'+
-	                        res[i].introduce +
-	                    '</p>'+
-	                    '<p>'+
-	                        '<span class="_font_weigth">总寿命：'+ timelife +
-	                        '</span>'+
-	                        '天<span class="_float_right _font_weigth">剩余：'+ reday +
-	                        '天</span>'+
-	                    '</p>'+
-	                '</div>';
-	            }
-	        }else{//流量剩余比例小则显示流量
-	            if(res[i].filtername){
-	                moHTML += '<li class="_padding">'+
-	                    '<p><span class="">'+ res[i].filtername +
-	                    '</span><span class="_float_right">'+
-	                   	(reflowpercent?reflowpercent:0) +
-	                    '%</span></p>'+
-	                    '<p class="_text_align">'+
-	                        '<div class="progress _progress _width">'+
-	                           '<div class="iconfont icon-kuang1"><span name='+ 'filter'+i +'></span></div>'+
-	                          '<div class="progress-bar progress-bar-striped" role="progressbar" style="width:'+
-	                         (reflowpercent?reflowpercent:0) +
-	                          '%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>'+
-	                        '</div>'+
-	                    '</p>'+
-	                '</li>';
-	            }
-	            //滤芯简介
-	            if(res[i].introduce){
-	                lvxinIntroduceHTML += '<div>'+
-	                    '<h3>'+
-	                        res[i].filtername +
-	                    '</h3>'+
-	                    '<p>'+
-	                        res[i].introduce +
-	                    '</p>'+
-	                    '<p>'+
-	                        '<span class="_font_weigth">总寿命：'+ flowlife +
-	                        '</span>'+
-	                        'L<span class="_float_right _font_weigth">剩余：'+ reflow +
-	                        'L</span>'+
-	                    '</p>'+
-	                '</div>';
-	            }
-	        }
-	    }
+		        //滤芯简介
+		        if(res[i].introduce && data.reflowfilter+[i+1]){
+		            lvxinIntroduceHTML += '<div>'+
+		                '<h3>'+
+		                    res[i].filtername +
+		                '</h3>'+
+		                '<p>'+
+		                    res[i].introduce +
+		                '</p>'+
+		                '<p>'+
+		                    '<span class="_font_weigth">总寿命：'+ flowlife +
+		                    'L</span>'+
+		                    '<span class="_float_right _font_weigth">剩余：'+ reflow +
+		                    'L</span>'+
+		                '</p>'+
+		            '</div>';
+		        }
+		    }else if(filtermode == "2"){//时常&流量
+		        // 遍历膜的详情, 不存在则不遍历
+		       if(redaypercent > reflowpercent){//时间剩余比例小则显示时间
+		            if(res[i].filtername){
+		                moHTML += '<li class="_padding">'+
+		                    '<p><span class="">'+ res[i].filtername +
+		                    '</span><span class="_float_right">'+
+		                    (redaypercent?redaypercent:0) +
+		                    '%</span></p>'+
+		                    '<p class="_text_align">'+
+		                        '<div class="progress _progress _width">'+
+		                           '<div class="iconfont icon-kuang1"><span name='+ 'filter'+i +'></span></div>'+
+		                          '<div class="progress-bar progress-bar-striped" role="progressbar" style="width:'+
+		                         (redaypercent?redaypercent:0) +
+		                          '%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>'+
+		                        '</div>'+
+		                    '</p>'+
+		                '</li>';
+		            }
+		            //滤芯简介
+		            if(res[i].introduce){
+		                lvxinIntroduceHTML += '<div>'+
+		                    '<h3>'+
+		                        res[i].filtername +
+		                    '</h3>'+
+		                    '<p>'+
+		                        res[i].introduce +
+		                    '</p>'+
+		                    '<p>'+
+		                        '<span class="_font_weigth">总寿命：'+ timelife +
+		                        '</span>'+
+		                        '天<span class="_float_right _font_weigth">剩余：'+ reday +
+		                        '天</span>'+
+		                    '</p>'+
+		                '</div>';
+		            }
+		        }else{//流量剩余比例小则显示流量
+		            if(res[i].filtername){
+		                moHTML += '<li class="_padding">'+
+		                    '<p><span class="">'+ res[i].filtername +
+		                    '</span><span class="_float_right">'+
+		                   	(reflowpercent?reflowpercent:0) +
+		                    '%</span></p>'+
+		                    '<p class="_text_align">'+
+		                        '<div class="progress _progress _width">'+
+		                           '<div class="iconfont icon-kuang1"><span name='+ 'filter'+i +'></span></div>'+
+		                          '<div class="progress-bar progress-bar-striped" role="progressbar" style="width:'+
+		                         (reflowpercent?reflowpercent:0) +
+		                          '%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>'+
+		                        '</div>'+
+		                    '</p>'+
+		                '</li>';
+		            }
+		            //滤芯简介
+		            if(res[i].introduce){
+		                lvxinIntroduceHTML += '<div>'+
+		                    '<h3>'+
+		                        res[i].filtername +
+		                    '</h3>'+
+		                    '<p>'+
+		                        res[i].introduce +
+		                    '</p>'+
+		                    '<p>'+
+		                        '<span class="_font_weigth">总寿命：'+ flowlife +
+		                        '</span>'+
+		                        'L<span class="_float_right _font_weigth">剩余：'+ reflow +
+		                        'L</span>'+
+		                    '</p>'+
+		                '</div>';
+		            }
+		        }
+		    }
+		}
 	}
 	$("#lvxinDetailUL").html(moHTML);
 	$("#lvxinReset").html(moHTML);
@@ -336,7 +341,7 @@ $(function(){
 				reday = (dataList['ReDayFilter'+(i+1)]?dataList['ReDayFilter'+(i+1)]:0);
 				reflow = (data['reflowfilter'+(i+1)]?data['reflowfilter'+(i+1)]:0);
 				redaypercent = ((parseInt(dataList['ReDayFilter'+(i+1)])/res[i].timelife)*100).toFixed(2);
-				reflowpercent = ((parseInt(dataList['ReFlowFilter'+(i+1)])/res[i].flowlife)*100).toFixed(0);
+				reflowpercent = ((parseInt(dataList['ReFlowFilter'+(i+1)])/res[i].flowlife)*100).toFixed(2);
 
 				if(parseInt(dataList['ReDayFilter1']) > 0 && parseInt(dataList['ReFlowFilter1']) <= 0 ){//按时间
 					// 遍历膜的详情, 不存在则不遍历
@@ -359,10 +364,7 @@ $(function(){
 					
 					//滤芯简介
 					if(res[i].introduce){
-
-						res[i].timelife = !res[i].timelife ? 0 : res[i].timelife;
 						dataList['ReDayFilter'+(i+1)] = !dataList['ReDayFilter'+(i+1)] ? 0 : dataList['ReDayFilter'+(i+1)];
-
 						lvxinIntroduceHTML += '<div>'+
 							'<h3>'+
 								res[i].filtername +
@@ -546,7 +548,6 @@ $(function(){
 						"Vison":0,
 						"PackNum":PackNum,
 					};
-					//alert(res[filterNumber].flowlife);
 					var type=0;
                     ajson['ReFlowFilter'+(filterNumber+1)]=res[filterNumber].flowlife;
                     ajson['ReDayFilter'+(filterNumber+1)]=res[filterNumber].timelife;
