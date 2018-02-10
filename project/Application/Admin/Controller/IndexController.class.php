ac614be7311ad8bb28ed2554d42a33d67425c95b
yi2018 <675218529@qq.com>
2018-02-10 15:24:50 +0800

    后台首页设备统计数据

--- project/Application/Admin/Controller/IndexController.class.php
+++ project/Application/Admin/Controller/IndexController.class.php
@@ -15,6 +15,8 @@ class IndexController extends CommonController
     	if (IS_AJAX) {
     		// 充值数额统计数量 （本月列表显示）
 	    	$flows = D('Flow')->getTotalByEveryDay();
+
+	    	$devices = D('Devices')->getTotalByEveryDay();
 	    	// 订单数量统计
 	    	$orders = D('ShopOrder')->field('count(distinct(order_id)) as total')->select();
 	    	// 保修数量统计->保修列表 
@@ -25,6 +27,7 @@ class IndexController extends CommonController
 
 	    	$data = [
 	    		'flows' => $flows,
+	    		'devices'=> $devices,
 	    		'orders' => $orders[0],
 	    		'repairs' => $repairs,
 	    		'feeds' => $feeds
