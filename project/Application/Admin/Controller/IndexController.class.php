446017d4bc9dbf6f45e04748dc86ed7c1e861dcc
yi2018 <675218529@qq.com>
2018-02-10 10:44:09 +0800

    后台首页数据统计

--- project/Application/Admin/Controller/IndexController.class.php
+++ project/Application/Admin/Controller/IndexController.class.php
@@ -12,17 +12,11 @@ class IndexController extends CommonController
 
     public function welcome()
     {
-    	// if (IS_AJAX) {
+    	if (IS_AJAX) {
     		// 充值数额统计数量 （本月列表显示）
 	    	$flows = D('Flow')->getTotalByEveryDay();
-
-	    	// 滤芯订单数量（已发货及未发货数量->以发货及未发货列表）
-	    	// $order_filters = D('Orders')
-	    	// 					->join('pub_express_information ON pub_orders.express_id = pub_express_information.id')
-	    	// 					->field('distinct(order_id)')
-	    	//                     ->select();
-	    	// $order_filter['total'] = count($order_filters); 
-
+	    	// 订单数量统计
+	    	$orders = D('ShopOrder')->field('count(distinct(order_id)) as total')->select();
 	    	// 保修数量统计->保修列表 
 	    	$repairs['total'] = D('Repair')->count();	    	
 
@@ -31,13 +25,15 @@ class IndexController extends CommonController
 
 	    	$data = [
 	    		'flows' => $flows,
-	    		// 'order_filters' => $order_filter,
+	    		'orders' => $orders[0],
 	    		'repairs' => $repairs,
 	    		'feeds' => $feeds
 	    	];
-	    	print_r($data);
+	    	// print_r($data);
 	    	$this->ajaxReturn($data);
-    	// }
-        $this->display('index');
+    	} else {
+    		$this->display();
+    	}
+        
     }
 }
