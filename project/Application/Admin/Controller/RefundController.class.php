cc9f2eb96a0df4c9201ab1ab8759035201795e6b
吴智彬 <519002008@qq.com>
2018-03-02 15:03:09 +0800

    我的订单售后报装列表数据

--- project/Application/Admin/Controller/RefundController.class.php
+++ project/Application/Admin/Controller/RefundController.class.php
@@ -6,6 +6,10 @@ use Think\Controller;
  */
 class RefundController extends CommonController
 {
+    /**
+     * 退货退款显示列表
+     * @return [type] 模板渲染
+     */
     public function index(){
         $data = D('Refund')->getPage(D('Refund'),'');
         // print_r($data);
@@ -13,7 +17,11 @@ class RefundController extends CommonController
         $this->display();
     }
 
-    public function showDetails()
+    /**
+     * 退货退款查看详情页
+     * @return [type] [description]
+     */
+    public function showDetails() 
     {
         $data = D('Refund')->relation(['logistics','goods'])->find(1);
         // dump($data);
@@ -21,6 +29,12 @@ class RefundController extends CommonController
         $this->display();
     }
 
+    /**
+     * 修改售后状态
+     * @param  int $id     退货退款id
+     * @param  int $status 状态
+     * @return [type]         [description]
+     */
     public function editStatus($id,$status)
     {
         if (is_numeric($id) && is_numeric($status)){
