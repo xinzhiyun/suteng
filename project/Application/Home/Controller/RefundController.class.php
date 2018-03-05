fc79451c2163ce4d8647144b0deba1d70feb124a
吴智彬 <519002008@qq.com>
2018-03-05 11:42:37 +0800

    退款

--- project/Application/Home/Controller/RefundController.class.php
+++ project/Application/Home/Controller/RefundController.class.php
@@ -10,7 +10,7 @@ class RefundController extends CommonController
 
     public function showGoods()
     {
-        // if (IS_AJAX) {
+        if (IS_AJAX) {
            $data = D('Refund')->relation(['goods'])->where(['uid'=>$_SESSION['user']['id']])->select();
 
             foreach ($data as $key => $value) {
@@ -25,13 +25,12 @@ class RefundController extends CommonController
                         ->find();
                } 
             }
-            print_r($data);
-            // if ($data) {
-            //    return $this->ajaxReturn(['code'=>200,'data'=>$data]);
-            // }else{
-            //    return $this->ajaxReturn(['code'=>400,'msg'=>'没有数据']);
-            // } 
-        // }
+            if ($data) {
+               return $this->ajaxReturn(['code'=>200,'data'=>$data]);
+            }else{
+               return $this->ajaxReturn(['code'=>400,'msg'=>'没有数据']);
+            } 
+        }
     }
 
     /**
