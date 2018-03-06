aa1a156416bb4f5270502f703d00f429964eb067
吴智彬 <519002008@qq.com>
2018-03-06 15:36:51 +0800

    后台报装列表

--- project/Application/Admin/Controller/RefundController.class.php
+++ project/Application/Admin/Controller/RefundController.class.php
@@ -50,4 +50,46 @@ class RefundController extends CommonController
         }
         
     }
+
+    public function install()
+    {
+        $map = '';
+        if (!empty(I('get.key')) && !empty(I('get.keywords'))) {
+            $map[I('get.key')] = array('like',"%".I('get.keywords')."%");
+        }
+        $data = D('shop_order')->alias('so')
+                            ->where(['gd.is_install'=>1])
+                            ->where($map)
+                            ->join('st_order_detail od ON od.order_id = so.order_id','LEFT')
+                            ->join('st_pic pic ON od.gid = pic.gid','LEFT')
+                            ->join('st_goods g ON od.gid = g.id','LEFT')
+                            ->join('st_users u ON so.uid = u.id','LEFT')
+                            ->join('st_goods_detail gd ON od.gid = gd.gid','LEFT')
+                            ->field(array('od.id'=>'id','so.uid','u.nickname','so.order_id','od.gid','od.num','od.price','g.name'=>'goodname','pic.path'=>'picpath','so.addtime','od.is_installed'))
+                            ->select();
+        // print_r($data);
+        $this->assign('data',$data);
+        $this->display();
+    }
+
+    /**
+     * 更改报装状态
+     * @param  int $id     order_detail的id
+     * @param  int $status is_installed
+     * @return json         [description]
+     */
+    public function updateInstalled($id,$status)
+    {
+        if (is_numeric($id) && is_numeric($status)){
+            $res = D('order_detail')->where('id='.$id)->setField('is_installed',$status);
+            if ($res) {
+                return $this->ajaxReturn(['code'=>200,'msg'=>'修改成功']);
+            } else{
+                return $this->ajaxReturn(['code'=>400,'msg'=>'修改失败']);
+            }
+            
+        } else{
+            return $this->ajaxReturn(['code'=>400,'msg'=>'id和状态必须为数字']);
+        }
+    }
 }
