9f5e04530123ce35466cb6ca76574c09e3777f63
lixiaoshe <25609587093@qq.com>
2018-03-09 15:11:02 +0800

    no message

--- project/Application/Admin/Controller/UsersController.class.php
+++ project/Application/Admin/Controller/UsersController.class.php
@@ -39,7 +39,7 @@ class UsersController extends CommonController
         $where['nickname'] = $_GET['nickname'];
         $where['grade'] = I('get.grade');
         $where['sex'] = I('get.sex');
-        dump($where);
+     
         $this->assign($assign);
         $this->assign('where',$where);
         $this->display();
