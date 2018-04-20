eb8f621ac77edd90c8f7ee4be851bf70591db61e
梁鼠标 <liangshubiao@liangshubiaodeMacBook-Pro.local>
2018-04-20 14:41:03 +0800

    梁康伦的修改—订单确认支付修改支付控制器

--- project/Application/Home/Controller/WeiXinPayController.class.php
+++ project/Application/Home/Controller/WeiXinPayController.class.php
@@ -137,6 +137,10 @@ class WeiXinPayController extends Controller
         // file_put_contents('./wx_dddpay.txt',$xml."\r\n", FILE_APPEND);die;
         if($xml){
 
+
+
+
+
             //解析微信返回数据数组格式
             $result = $this->notifyData($xml);
             // dump($result);die;
@@ -202,6 +206,119 @@ class WeiXinPayController extends Controller
             }
         }
     }
+
+    /*********************************************新增方法-支付后修改订单数据**********************************************/
+
+        /**
+     * 处理订单写入数据
+     * @return array 返回数组格式的notify数据
+     */
+    public function onotify()
+    {
+        // 获取微信服务器返回的xml文档
+//        $xml=file_get_contents('php://input', 'r');
+//        Log::write( $xml,'测试一号');
+        $xml = '<xml><appid><![CDATA[wx676721599e5766c0]]></appid>
+<attach><![CDATA[819818675126975]]></attach>
+<bank_type><![CDATA[CFT]]></bank_type>
+<cash_fee><![CDATA[1]]></cash_fee>
+<fee_type><![CDATA[CNY]]></fee_type>
+<is_subscribe><![CDATA[Y]]></is_subscribe>
+<mch_id><![CDATA[1501254081]]></mch_id>
+<nonce_str><![CDATA[5wgcm1ptarl19i0v3k06k93p8osbderw]]></nonce_str>
+<openid><![CDATA[onLe70fYcrqU71RjzfYUjkNf90_E]]></openid>
+<out_trade_no><![CDATA[841179983093492]]></out_trade_no>
+<result_code><![CDATA[SUCCESS]]></result_code>
+<return_code><![CDATA[SUCCESS]]></return_code>
+<sign><![CDATA[CA0B6D87B669E772C9297B65832B3EA2]]></sign>
+<time_end><![CDATA[20180411145314]]></time_end>
+<total_fee>1</total_fee>
+<trade_type><![CDATA[JSAPI]]></trade_type>
+<transaction_id><![CDATA[4200000052201804116501776653]]></transaction_id>
+</xml>
+';
+
+// 
+// 
+// UR体会与人体热敷的供热的高
+        // file_put_contents('./wx_dddpay.txt',$xml."\r\n", FILE_APPEND);die;
+        if($xml){
+
+            
+
+
+
+            //解析微信返回数据数组格式
+            $result = $this->notifyData($xml);
+            // dump($result);die;
+            // 实例化订单模型
+            $order = M('shop_order');
+            // 准备查询订单的条件
+            $showOrder['order_id'] = $result['attach'];
+            // 查询订单表
+            $orderData = $order->where($showOrder)->find();
+
+            
+            // 如果订单未支付
+            if($orderData['status']==8){
+
+                // 开启事务
+                // $order->startTrans();
+                // dump();die;
+                $saveOrder['order_id']  = $orderData['order_id'];
+                // 用户ID
+                $showAddres['uid']      = $orderData['uid'];
+                // 默认的地址
+                $showAddres['status']   = 0;
+                // 快递地址ID
+                $saveOrderDara['address_id'] = M('address')->where($showAddres)->find()['id'];
+                // 准备更新数据
+//                $saveOrderDara['status'] = 9;
+                // 支付类型
+                $saveOrderDara['mode'] = 0;
+                // 执行更新操作
+                $order->where($saveOrder)->save($saveOrderDara);
+                // 准备佣金数据
+                // 微信用户标识
+                $open_id = $result['openid'];
+                // 订单编号
+                $order_id = $result['attach'];
+
+                // 总成本
+                $cost = $orderData['g_cost'];
+                // 总售价
+                $price = $orderData['g_price'];
+                // 总利润
+                $profit = $price - $cost;
+                // 佣金比例
+                $yjbl = 0.03;
+                // 金币比例
+                $jbbl = 0.01;
+                // 银币比例
+                $ybbl = 0.02;
+
+                // 佣金
+                $yj = ($profit*$yjbl)>0?($profit*$yjbl):0;
+                // 金币
+                $jb = ($profit*$jbbl)>0?($profit*$jbbl):0;
+                // 银币
+                $yb = ($profit*$ybbl)>0?($profit*$ybbl):0;
+                //商品销售佣金
+                $list = M('ShopOrder as a')->field('a.id,a.order_id,a.gid,a.g_cost,a.g_price,a.g_num,b.vid,c.id ccid,c.code,c.invitation_code,c.superiors_code,c.superior_code,c.abonus')->join('st_goods b on a.gid = b.id')->join('st_vendors c on b.vid = c.id')->where(['a
+                .g_type'=>1,'a.status'=>7,'a.order_id'=>$saveOrder['order_id']])->select();
+                echo M('ShopOrder as a')->getLastSql();
+                   // echo $open_id.'-'.$yj.'-'.$jb.'-'.$yb;die;
+                // 分配佣金
+                $this->branch_commission($open_id,$order_id,$yj,$jb,$yb);
+            }
+        }
+    }
+
+
+    /*********************************************新增结束**********************************************/
+
+
+
 
     /**
      * [vendorNotify 分销商加盟费回调]
