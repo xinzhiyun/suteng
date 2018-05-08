87ba486108acfa3d26048c986e9718fcea021458
XUELINYU <570600268@qq.com>
2018-05-08 14:03:11 +0800

    添加后台滤芯列表提示信息

--- project/Application/Admin/Model/FiltersModel.class.php
+++ project/Application/Admin/Model/FiltersModel.class.php
@@ -6,6 +6,7 @@ class FiltersModel extends BaseModel
     // 自动验证
     protected $_validate = array(
         array('filtername','require','滤芯名称不能为空'),
+        array('alias','require','滤芯别名不能为空'),
         // array('filtername','/^[a-zA-Z0-9\x{4e00}-\x{9fa5}]{1,30}$/u','滤芯名称格式不正确',1,'regex'),
         // array('alias','/^[a-zA-Z0-9\x{4e00}-\x{9fa5}]{0,30}$/u','滤芯别名格式不正确',1,'regex'),
         array('timelife','/^\+?[1-9][0-9]*$/','请输入有效数值',1,'regex'),
