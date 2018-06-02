b8d09a217a7fffe28f16d084bffc331583accb65
李振东 <lzdong@foxmail.com>
2018-06-02 11:52:11 +0800

    雷振东的修改—工单接口

---
+++ project/Application/Home/Controller/WorkController.class.php
@@ -0,0 +1,51 @@
+<?php
+namespace Home\Controller;
+
+class WorkController extends CommonController
+{
+    /**
+     * 发起安装工单
+     */
+    public function workAdd()
+    {
+        try {
+            $post = I('post.');
+            if (empty($post['sid'])) {
+                E('请选择服务站', 40103);
+            }
+            if (empty($post['type'])) {
+                E('请选择工单', 40102);
+            } else {
+                $data['type'] = $post['type'];
+            }
+            if (empty($post['orderid'])) {
+                E('订单号为空', 40101);
+            } else {
+                $data['orderid'] = $post['orderid'];
+            }
+
+            $orderMap=array(
+                'order_id'=>$post['orderid'],
+                ''
+            );
+            M('orders')->where($orderMap)->find();
+
+            $data['number'] = getWorkNumber();
+
+            if($data['type']==0){
+                $data['content'] = '新购设备-安装';
+                $data['title']   = '设备安装';
+            }
+            $data['create_at']=time();
+            $data['update_at']=time();
+            M('work')->add($data);
+
+            E('工单开启成功,请等待安装服务人员联系',200);
+        } catch (\Exception $e) {
+            $this->toJson($e);
+        }
+    }
+    
+}   
+
+
