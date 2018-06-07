02af9db13fb4de64433c7a21eb1d2fe6da295853
李振东 <lzdong@foxmail.com>
2018-06-07 15:35:16 +0800

    雷振东的修改—

--- project/Application/Home/Controller/ServiceCommonController.class.php
+++ project/Application/Home/Controller/ServiceCommonController.class.php
@@ -22,12 +22,11 @@ class ServiceCommonController extends Controller
 
         // 后续添加 绑定微信进行自动登录
 
-        $this->redirect("Home/SericeLogin/index");
 
-
+        if(empty($_SESSION['serviceInfo'])){
+            $this->redirect("Home/SericeLogin/index");
+        }
     }
-
-
 
 
 
