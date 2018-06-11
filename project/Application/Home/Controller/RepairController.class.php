0f975bd5eddcb10b5855e19ddc6c3d3e4f6ec747
李振东 <lzdong@foxmail.com>
2018-06-11 18:06:18 +0800

    雷振东的修改—安装数据

--- project/Application/Home/Controller/RepairController.class.php
+++ project/Application/Home/Controller/RepairController.class.php
@@ -12,13 +12,6 @@ class RepairController extends CommonController
     {
         $map['uid'] = session('user.id');
 
-        $maps['uid'] = session('user.id');
-        $maps['status'] = I('status',0);
-
-
-        $installList = M('shop_order_device_install')->where($maps)->select();
-        $this->assign('installList',$installList);
-
 
         // 调试用默认用户
         $openId = $_SESSION['user']['open_id'];
@@ -32,6 +25,18 @@ class RepairController extends CommonController
         $weixin = new WeixinJssdk;
         $signPackage = $weixin->getSignPackage();
         $this->assign('info',$signPackage);
+
+        $this->display();
+    }
+    
+    // 安装
+    public function install()
+    {
+        $maps['uid'] = session('user.id');
+        $maps['status'] = I('status',0);
+
+        $installList = M('shop_order_device_install')->where($maps)->select();
+        $this->assign('installList',$installList);
 
         $this->display();
     }
