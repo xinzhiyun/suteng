f69a6a55db84636d7a74baa7de3c11f8e7a69f0a
李振东 <lzdong@foxmail.com>
2018-06-13 22:41:29 +0800

    雷振东的修改—用户

--- project/Application/Home/Controller/ServicePeopleController.class.php
+++ project/Application/Home/Controller/ServicePeopleController.class.php
@@ -18,23 +18,26 @@ class ServicePeopleController extends Controller
     }
 
     public function index(){
-        $company = M('service')->cache('cache_company_name',60)->where('id='.$_SESSION['servicepeople']['sid'])->getField('company');
-        $_SESSION['servicepeople']['company']=$company;
+        $_SESSION['servicepeople']['company'] = M('service')
+            ->cache('cache_company_name',60)
+            ->where('id='.$_SESSION['servicepeople']['sid'])
+            ->getField('company');
+
+        $work_model = M('work');
+
+        $map = [
+            'sid'=>$_SESSION['servicepeople']['sid'],
+            'dw_uid'=>$_SESSION['servicepeople']['id'],
+            'result'=>1
+        ];
+        $work['wait_install']  = $work_model->where($map)->where(['type'=>0])->count();
+        $work['wait_repair']   = $work_model->where($map)->where(['type'=>1])->count();
+        $work['wait_maintain'] = $work_model->where($map)->where(['type'=>2])->count();
+        $this->assign('work',$work);
         $this->display();
     }
 
-    // 任务列表
-    public function servicePersonal()
-    {
-        $this->display();
-    }
-    // 任务列表
-    public function list()
-    {
-        $this->display('service/list');
-    }
-
-    // 获取列表
+    // 获取服务(任务)列表
     public function getList()
     {
         $p = I('p',1);
@@ -42,31 +45,53 @@ class ServicePeopleController extends Controller
 
         $map['sid'] = $_SESSION['serviceInfo']['id'];
 
-        $total = M('service_users')
+        strlen(I('type'))?$map['type'] = I('type'):'';
+        $total = M('work')
             ->where($map)
-            ->count();
+            ->count('id');
         if(empty($total)){
             $this->toJson(['data'=>[]],'获取成功');
         }
 
         $page  = new \Think\Page($total,10);
-        $list = M('service_users')
+        $list = M('work')
             ->where($map)
             ->limit($page->firstRow.','.$page->listRows)
+            ->field('id,kname,kphone,create_at')
             ->select();
         $this->toJson(['data'=>$list],'获取成功');
     }
 
-    // 工单完成
-    public function packwork()
+    // 获取工单详情
+    public function getDetail()
     {
         try {
             $post = I('post.');
-            if( empty($post['number']) ){
+            if( empty($post['wid']) ){
                 E('工单信息错误',400022);
             }
 
-            $map['number'] = $post['number'];
+            $map['id'] = $post['wid'];
+            $info = M('work')->where($map)->find();
+            if( empty($info) ){
+                E('工单不存在',400022);
+            }
+            $this->toJson(['data'=>$info,200]);
+        } catch (\Exception $e) {
+            $this->toJson($e);
+        }
+    }
+
+    // 工单完成
+    public function passWork()
+    {
+        try {
+            $post = I('post.');
+            if( empty($post['wid']) ){
+                E('工单信息错误',400022);
+            }
+
+            $map['id'] = $post['wid'];
             $info = M('work')->where($map)->find();
             if( empty($info) ){
                 E('工单不存在',400022);
@@ -77,7 +102,7 @@ class ServicePeopleController extends Controller
             $res = M('work')->where($map)->save($data);
 
             if ($res) {
-                Work::add($info['id'], 5); //完成
+                Work::add($info['id'], 7); //完成
                 E('提交成功!',200);
             }else{
                 E('提交失败,请重试!',400001);
@@ -86,6 +111,10 @@ class ServicePeopleController extends Controller
             $this->toJson($e);
         }
     }
+
+
+    // 代缴费 搜索
+    
 
 
 
