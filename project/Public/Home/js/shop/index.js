b753d2ed94672b1538b6fd8981471f397f686647
zero9527 <zero8297183@163.com>
2018-06-29 11:47:28 +0800

    修改购物车选择

--- project/Public/Home/js/shop/index.js
+++ project/Public/Home/js/shop/index.js
@@ -4,8 +4,8 @@ var shopindex = new Vue({
 		return {
 			menuList: [],				// 分类按钮
 			blockList: [],				// 商品块集合
-			classifyList: [],			// 一级分类类目
-			classifyContentList: [],	// 二级分类
+			categoryList: [],			// 一级分类类目
+			categoryContentList: [],	// 二级分类
 			cartList: [],				// 购物车商品集合
 			cateSelect: '',
 			tabclk: 1, 					// 底部按钮
@@ -13,18 +13,21 @@ var shopindex = new Vue({
 			tabsrc: [],
 			srcArr: [
 				public + '/Home/images/shop/home',
-				public + '/Home/images/shop/classify',
+				public + '/Home/images/shop/category',
 				public + '/Home/images/shop/addcart',
 				public + '/Home/images/shop/mine'
 			],
 			titleList: ['商城首页','分类','购物车','我的'],
+			categoryTitle: '',  //二级分类标题
+			noCateContent: false,
+			cart_none: '',
 		}
 	},
 	created() {
 		var vm = this;
 		var href = location.href;
 		// 刷新保持状态
-		// 首页_home, 分类classify, 购物车cart, 我的mine
+		// 首页_home, 分类category, 购物车cart, 我的mine
 		for(var i=0; i<vm.srcArr.length; i++){
 			vm.tabsrc.push(vm.srcArr[i])
 		}
@@ -32,8 +35,8 @@ var shopindex = new Vue({
 		if(href.indexOf('_home') > 0){
 			vm.tabClick(1, '_home');
 
-		}else if(href.indexOf('classify') > 0){
-			vm.tabClick(2, 'classify');
+		}else if(href.indexOf('category') > 0){
+			vm.tabClick(2, 'category');
 
 		}else if(href.indexOf('cart') > 0){
 			vm.tabClick(3, 'cart');
@@ -90,14 +93,15 @@ var shopindex = new Vue({
 		];
 
 		// 一级分类
-		vm.classifyList = [
-			{name: '热水器', cid: '12'},
-			{name: '净水器', cid: '13'},
-			{name: '瓦胆系列', cid: '142'},
-			{name: '空气净化', cid: '14'},
-			{name: '生活用品', cid: '112'},
-			{name: '数码家电', cid: '242'}
-		]
+		vm.categoryList = JSON.parse(category);
+		// vm.categoryList = [
+		// 	{name: '热水器', cid: '12'},
+		// 	{name: '净水器', cid: '13'},
+		// 	{name: '瓦胆系列', cid: '142'},
+		// 	{name: '空气净化', cid: '14'},
+		// 	{name: '生活用品', cid: '112'},
+		// 	{name: '数码家电', cid: '242'}
+		// ]
 
 		// 购物车数据
 		vm.cartList = [
@@ -128,50 +132,113 @@ var shopindex = new Vue({
 		},
 		// 点击分类类目
 		getCate(cate, cid) {
+			var vm = this;
+			vm.categoryTitle = cate.name;
 			// 点击同一个分类类目
 			if(window.clickCid == cid) return;
 			window.clickCid = cid;
-			this.cateSelect = cate;
+			vm.cateSelect = cate;
 			console.log('cid: ',cid);
 			// 获取对应类目下的商品详细分类
-			// $.ajax({
-			// 	url: '',
-			// 	type: 'get',
-			// 	data: {cid: cid},
-			// 	success: function(res){
-			// 		console.log('res: ',res);
-			// 	},
-			// 	error: function(err){
-			// 		console.log('err: ',err);
-			// 	}
-			// })
-
-			this.classifyContentList = [
-				{
-					title: '净水器',
-					subType: [
-						{src: '',name:'lopo玩具',scid: '12'},
-						{src: '',name:'毛衣刷',scid: '13'},
-						{src: '',name:'宝宝杯',scid: '14'},
-						{src: '',name:'小小苏',scid: '15'},
-						{src: '',name:'规划局规划国际化',scid: '25'},
-					]
+			$.ajax({
+				url: getURL('Home','Shop/getCategory'),
+				type: 'post',
+				data: {id: cid},
+				success: function(res){
+					console.log('res: ',res);
+					if(res.status == 200){
+						vm.categoryContentList = res.data;
+						vm.noCateContent = false;
+						if(!res.data.length){
+							vm.noCateContent = true;
+						}
+					}else{
+						layuiHint(res.msg);
+						vm.noCateContent = true;
+					}
 				},
-				{
-					title: '净化器',
-					subType: [
-						{src: '',name:'lopo玩具',scid: '12'},
-						{src: '',name:'毛衣刷',scid: '13'},
-						{src: '',name:'宝宝杯',scid: '14'},
-						{src: '',name:'小小苏',scid: '15'},
-						{src: '',name:'规划局规划国际化',scid: '25'},
-					]
+				error: function(err){
+					console.log('err: ',err);
 				}
-			];
+			})
+
+			// this.categoryContentList = [
+			// 	{
+			// 		title: '净水器',
+			// 		subType: [
+			// 			{src: '',name:'lopo玩具',scid: '12'},
+			// 			{src: '',name:'毛衣刷',scid: '13'},
+			// 			{src: '',name:'宝宝杯',scid: '14'},
+			// 			{src: '',name:'小小苏',scid: '15'},
+			// 			{src: '',name:'规划局规划国际化',scid: '25'},
+			// 		]
+			// 	},
+			// 	{
+			// 		title: '净化器',
+			// 		subType: [
+			// 			{src: '',name:'lopo玩具',scid: '12'},
+			// 			{src: '',name:'毛衣刷',scid: '13'},
+			// 			{src: '',name:'宝宝杯',scid: '14'},
+			// 			{src: '',name:'小小苏',scid: '15'},
+			// 			{src: '',name:'规划局规划国际化',scid: '25'},
+			// 		]
+			// 	}
+			// ];
 		},
 		// 点击大分类下的小分类商品图片
 		subClick(scid) {
 			console.log('scid: ',scid);
-		}
+		},
+		// 购物车商品左滑
+		slideDelete(e, gid) {
+			var el = e.currentTarget;
+			// console.log('gid: ',gid);
+			// console.log('e: ',e);
+			console.group();
+			if(!window.touchX){
+				window.touchX = e.changedTouches[0].pageX;
+				console.log('touchX: ',window.touchX);
+			}else{
+				if(window.touchX - e.changedTouches[0].pageX < 0 && window.touchX - e.changedTouches[0].pageX <= -30){
+					console.clear();
+					console.log('右滑');
+					el.setAttribute('style','transform:translateX(0);');
+					window.isDeleteShow = true;
+					window.touchX = '';
+					
+				}else if(window.touchX - e.changedTouches[0].pageX > 0 && window.touchX - e.changedTouches[0].pageX >= 30){
+					console.clear();
+					console.log('左滑');
+					el.setAttribute('style','transform:translateX(-40px);');
+					window.isDeleteShow = false;
+					window.touchX = '';
+				}
+			}
+			console.log('window.touchX - e.changedTouches[0].pageX: ',window.touchX - e.changedTouches[0].pageX);
+			console.groupEnd();
+		},
+		slideEnd() {
+			window.touchX = '';
+		},
+		// 删除购物车商品
+		deleteCart(gid) {
+			console.log('gid: ',gid);
+		},
+		// 购物车勾选
+		cartSelect(gid, price, index, e) {
+			var vm = this;
+			var el = e.currentTarget;
+			var src = el.querySelector('img').getAttribute('src');
+			//gid商品id, price单价, index序号
+			console.log('gid: %s, price: %s, index: %s',gid, price, index);
+			if(src.indexOf('select') < 0){	// 选中
+				var _src = public+'/Home/images/shop/cart_select.png';
+				el.querySelector('img').setAttribute('src', _src)
+
+			}else{	// 取消选中
+				var _src = public+'/Home/images/shop/cart_none.png';
+				el.querySelector('img').setAttribute('src', _src)
+			}
+		}	
 	}
 })
