babdd4086259025f7d14ace191452f8f4dfed942
zero9527 <zero8297183@163.com>
2018-07-01 21:14:59 +0800

    修改商城首页，后台商品添加属性选择组合

--- project/Public/Home/js/shop/shopdetailx.js
+++ project/Public/Home/js/shop/shopdetailx.js
@@ -3,8 +3,10 @@ var vm = new Vue({
     data() {
         return {
             goodsInfo: {},
-            gocart: false,//立即购买/加入购物车/
-            DisFlag : 0,//选项卡
+            gocart: false,      //立即购买/加入购物车/
+            DisFlag : 0,        //选项卡
+            checkList: '',      // 选择的套餐规格
+            lastCheckPname: '',    // 上次选择的规格
             goodDetail: [
                 {src :public + "/Home/images/1a.jpg"},
                 {src :public + "/Home/images/1a.jpg"},
@@ -33,40 +35,45 @@ var vm = new Vue({
         // 默认选中第一个
         $(".titleKa>li>span").eq(0).addClass("selecteds");
         // 模拟
-        this.goodsInfo = {
-            goodsid: '',            // 商品编号
-            name: '速腾智能净水机H款',      // 商品名称
-            desc: '全新智能升级的净水机',   // 商品描述
-            rate: '98%',            // 好评率
-            newprice: '1398.00',    // 打折价格？会员价格
-            price: '1398.00',       // 旧价格
-            leaseprice: '300/月',   // 租赁价格
-            subjoin: '购买送1000银币',
-            postage: '0.00',        // 邮费
-            address: '广东中山',    // 发货地址？产地？
-            buyattr: [
-                // 购买规格选项
-                {
-                    title: '颜色',
-                    id: 1,
-                    data: [
-                        {
-                            name: '高压白',
-                            id: '11'
-                        }
-                    ]
-                }
-            ],               
-            showattr: [                // 显示商品规格
-                {
-                    title: '尺寸',          // 名称
-                    content: '14'         // 内容
-                },
-                {
-                    title: '重量',          // 名称
-                    content: '4kg'         // 内容
-                }
-            ]
+        // this.goodsInfo = {
+        //     goodsid: '',            // 商品编号
+        //     name: '速腾智能净水机H款',      // 商品名称
+        //     desc: '全新智能升级的净水机',   // 商品描述
+        //     rate: '98%',            // 好评率
+        //     newprice: '1398.00',    // 打折价格？会员价格
+        //     price: '1398.00',       // 旧价格
+        //     leaseprice: '300/月',   // 租赁价格
+        //     subjoin: '购买送1000银币',
+        //     postage: '0.00',        // 邮费
+        //     address: '广东中山',    // 发货地址？产地？
+        //     buyattr: [
+        //         // 购买规格选项
+        //         {
+        //             title: '颜色',
+        //             id: 1,
+        //             data: [
+        //                 {
+        //                     name: '高压白',
+        //                     id: '11'
+        //                 }
+        //             ]
+        //         }
+        //     ],               
+        //     showattr: [                // 显示商品规格
+        //         {
+        //             title: '尺寸',          // 名称
+        //             content: '14'         // 内容
+        //         },
+        //         {
+        //             title: '重量',          // 名称
+        //             content: '4kg'         // 内容
+        //         }
+        //     ]
+        // }
+        var gid = GetQueryString('gid');
+        if(gid){
+            // 获取商品数据
+            this.getDetail(gid);
         }
     },
     methods: {
@@ -84,13 +91,48 @@ var vm = new Vue({
                 vm.DisFlag = $(target).attr("index");
             }
         },
-        // 选中规格和颜色
-        choice() {
+        // 选中规格
+        choice(id, pname, name, e) {
+            // id:属性id, pname:父级, name:属性名
             var vm = this;
-            var ev = ev || window.event;
+            var ev = e || window.event;
             var target = ev.target || ev.srcElement;
             console.log(target);
             $(target).css("border", "1px solid #518CF8").parent().siblings().children().css("border", "1px solid #747474");
+            // 已选规格
+            if(vm.lastCheckPname == pname){
+                vm.checkList = name;
+            }else{
+                vm.checkList = name;
+            }
+            // 记录本地点击的id
+            vm.lastCheckPname = pname;
         },
+        getDetail(gid) {
+            var vm = this;
+            console.log('gid: ',gid);
+            $.ajax({
+                url: getDetailURL,
+                type: 'post',
+                data: {id: gid},
+                success: function(res){
+                    console.log('res: ',res);
+                    if(res.status == 200){
+                        vm.goodsInfo = res.data;
+                        vm.$nextTick(function(){
+                            // 图片懒加载
+                            lazyLoad('.swiper-wrapper');
+                        })
+                    }
+                },
+                error: function(err){
+                    console.log('err: ',err);
+                }
+            })
+        },
+        // 去购物车
+        cart() {
+            location.href = shopindex + '#_cart';
+        }
     },
 })
