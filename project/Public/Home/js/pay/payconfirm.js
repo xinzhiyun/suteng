435dfedcc13434418be4db2cf8ba5ffa1b760f75
zero9527 <zero8297183@163.com>
2018-07-02 21:18:41 +0800

    修改订单确认提交哦

--- project/Public/Home/js/pay/payconfirm.js
+++ project/Public/Home/js/pay/payconfirm.js
@@ -3,87 +3,96 @@ var payConfirm = new Vue({
 	data() {
 		return {
 			info: {},		// 所有数据
+			goods: [],		// 订单数据
 			allMoney: 0,	// 总价格
-			address: true,
 			express: [],
 			einfo: [],	// 选择的快递信息
 		}
 	},
 	created() {
 		var vm = this;
-		vm.express = [
-			{name: '中通快递', money: '12'},
-			{name: '申通快递', money: '16'},
-			{name: '圆通快递', money: '17'},
-		]
-		vm.express = info.goodsCourier['14'];
 		vm.info = info;
-		// 模拟
-		vm.info = {
-			user: {
-				name: '小鸡壳',
-				phone: '15315645789',
-				address: '广州市番禺区大夫山北段362号钟村镇点球电子科技集团'
-			},
-			order: {
-				money: '189',	// 合计价格
-				postage: '8',	// 邮费
-				express: '',	// 快递
-				sendAddr: '',	// 发货地
-				integral: '',	// 所需积分
-				goods: [
-					{
-						id: '11',
-						pic: '1111111',
-						name: '滤芯外部活性炭',
-						skuattr: '',
-						price: '119',
-						num: '1'
-					},
-					{
-						id: '12',
-						pic: '22222222',
-						name: '滤芯外部活性炭',
-						skuattr: '',
-						price: '119',
-						num: '1'
-					}
-				]
+		// 请求订单数据
+		vm.getOrderInfo();
+		// 发票
+		if(sessionStorage.getItem("voiceArr")){
+			voiceArr = JSON.parse(sessionStorage.getItem("voiceArr"));
+			if(voiceArr.type){
+				$(".ynticket>a>span").text(voiceArr.type)
 			}
-		};
-		var goods = vm.info.order.goods;
-		// 计算总价格
-		for(var i=0; i<goods.length; i++){
-			vm.allMoney += (+goods[i].price)*(+goods[i].num);
+			$('.invoice .icon-weixuanzhong').attr('iconfont icon-xuanze');
+		}else{
+			$('.invoice .icon-xuanze').attr('iconfont icon-weixuanzhong');
 		}
 	},
 	methods: {
 		// 请求订单数据
 		getOrderInfo() {
+			var vm = this;
 			$.ajax({
-				url: payConfirm,
+				url: getGoodsByOrder,
 				type: 'get',
 				success: function(res){
 					if(res.code == 200){
-						// vm.info = res.data;
+						vm.goods = res.msg;
+						// 默认第一个快递
+						for(var i=0; i<res.msg.length; i++){
+							vm.einfo.push({
+								cid: res.msg[i].Courier[0].cid,
+								cname: res.msg[i].Courier[0].cname,
+								cprice: res.msg[i].Courier[0].cprice,
+							})
+						}
+						// 计算价格
+						vm.calcAllmoney();
 					}
 				},
 				error: function(err){}
 			})
 		},
 		// 快递选择
-		expressChange(){
+		expressChange(index){
 			var vm = this;
 			var express = $('.express');
+			vm.einfo = [];
 			for(var i=0; i<express.length; i++){
 				var temp = $(express[i]).find('option:selected');
 				vm.einfo.push({
-					id: temp[0].getAttribute('eid'),
-					name: temp[0].getAttribute('value'),
+					gid: vm.goods[index].gid,
+					ordre_id: GetQueryString('ordre_id'),
+					cid: temp[0].getAttribute('eid'),
+					cname: temp[0].getAttribute('value'),
+					cprice: temp[0].getAttribute('price')
 				})
 				console.log('temp: ',temp);
 			}
 			console.log('vm.einfo: ',vm.einfo);
+			// 计算价格
+			vm.calcAllmoney();
+			$.ajax({
+				url: updateOrder,
+				data: {postage: vm.einfo},
+				type: 'post',
+				success: function(res){
+					console.log('res: ',res);
+				},
+				error: function(err){}
+			})
+		},
+		// 计算总价格
+		calcAllmoney() {
+			var vm = this;
+			vm.allMoney = 0;
+			// 商品价格
+			for(var i=0; i<vm.goods.length; i++){
+				vm.allMoney += (+vm.goods[i].price)*(+vm.goods[i].num);
+			}
+			// 快递费
+			for(var j=0; j<vm.einfo.length; j++){
+				for(var k=0; k<vm.goods.length; k++){
+					vm.allMoney += (+vm.einfo[j].cprice)*(+vm.goods[k].num);
+				}
+			}
 		}
 	}
 })
